5e9bbb8d4ae8941de5e38e44cba716993d868850
AntLinux <anton-r@live.com>
2021-05-18 12:16:48 +0300

    some why i cant trasfer df.to_htl object as {} notation , utils.py 65 line . transfer as tuple

--- sales/utils.py
+++ sales/utils.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.db.models import Q
 
 from customers.models import Customer
@@ -5,13 +7,18 @@ from profiles.models import Profile
 from sales.models import Sale
 import pandas as pd
 from django.contrib import messages
+from io import BytesIO
+import matplotlib.pyplot as plt
+
+# global variables
+merge_df = None
 
 
 # work with database
 def sales_and_positions(date_from, date_to, chart_type=None, request=None) -> dict or None:
     sales_df = None
     positions_df = None
-    merge_df = None
+    global merge_df
     positions_list = []
 
     # load data to Pandas DataFrame for calculations
@@ -50,25 +57,32 @@ def sales_and_positions(date_from, date_to, chart_type=None, request=None) -> di
             "id": "sale_id"
         }, axis=1, inplace=True)
 
+        # created one big data source with all variables
         merge_df = pd.merge(sales_df, positions_df, on="sale_id")
 
+        # detailed view of big data
         by_id = merge_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
         by_customer = merge_df.groupby('customer', as_index=False)['price'].agg('sum')
         by_sales_man = merge_df.groupby('sales man', as_index=False)['price'].agg('sum')
 
-        # amazing bootstrap classes (its a free magic)
+        # amazing bootstrap classes (its a free magic), convert data to html tables
         main_df_html = merge_df.to_html(classes=["table-bordered", "table-striped", "table-hover", "table"])
         by_id_html = by_id.to_html(classes=["table-bordered", "table-striped", "table-hover", "table"])
         by_customer_html = by_customer.to_html(classes=["table-bordered", "table-striped", "table-hover", "table"])
         by_sales_man_html = by_sales_man.to_html(classes=["table-bordered", "table-striped", "table-hover", "table"])
 
-        return main_df_html, by_id_html, by_customer_html, by_sales_man_html
+        # creating chart based on the user choose
+        chart = get_chart(chart_type, by_id, labels=by_id['transaction_id'].values)
+
+        # return as tuple all data
+        return main_df_html, by_id_html, by_customer_html, by_sales_man_html, chart
 
     else:
         messages.warning(request, f'Sorry can not find data for this time period !')
         return None
 
 
+# Inner utils functions  ====================
 def _get_salesman_name(id: int) -> str:
     profile = Profile.objects.get(pk=id)
     return profile.user.username
@@ -77,3 +91,51 @@ def _get_salesman_name(id: int) -> str:
 def _get_customer_name(id: int) -> str:
     customer = Customer.objects.get(pk=id)
     return customer.name
+
+
+def _get_graph():
+    """
+    # have an 2 ways for do that one is using buffer in rum_memory (nice and fast)
+    # second for all query save chart and upload to the storage (not good decision)
+    """
+
+    # after getting data from user and figure type
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    img_png = buffer.getvalue()
+    # encode picture
+    graph = base64.b64encode(img_png)
+    # decode to html utf-8
+    graph = graph.decode('utf-8')
+    # close and return html friendly obj
+    buffer.close()
+    return graph
+
+
+def get_chart(chart_type, data, **kwargs):
+    """
+    by getting choose from user decide what kind of chart will return ,
+    as data will use [sum(prise) grouped by transaction_id]
+    """
+    plt.switch_backend('AGG')
+    plt.figure(figsize=(10, 4))
+    plt.tight_layout()
+
+    if chart_type == "1":
+        print("1 : Bar Chart")
+        plt.bar(data['transaction_id'], data['price'])
+
+    elif chart_type == "2":
+        print("2 : Pie Chart")
+        plt.pie(data=data, x='price', labels=kwargs['labels'])
+
+    elif chart_type == "3":
+        print("3 : Line Chart")
+        plt.plot(data['transaction_id'], data['price'])
+
+    else:
+        print(" -- some strange chart ! failed to identify it ...")
+
+    chart = _get_graph()
+    return chart
